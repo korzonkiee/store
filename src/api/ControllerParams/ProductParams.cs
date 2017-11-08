a80e006d3f10d5b3345d60485717fd846790dfe3
Korzonkie <rootie0011@gmail.com>
2017-11-08 12:45:08 +0100

    Add additional properties to Prodcut model

--- src/api/ControllerParams/ProductParams.cs
+++ src/api/ControllerParams/ProductParams.cs
@@ -3,5 +3,7 @@ namespace api.ControllerParams
     public class ProductParams
     {
         public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
     }
 }
--- src/core/models/Product.cs
+++ src/core/models/Product.cs
@@ -6,13 +6,19 @@ namespace core.models
     {
         public Guid Id { get; set; }
         public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public DateTime DateCreated { get; set; }
 
-        public static Product Create(string name)
+        public static Product Create(string name, string description, string imageUrl)
         {
             var product = new Product()
             {
                 Id = Guid.NewGuid(),
-                Name = name
+                Name = name,
+                ImageUrl = imageUrl,
+                Description = description,
+                DateCreated = DateTime.Now
             };
 
             return product;

1d3c4ad9f67693479279aa20d3cdf69c9878ad9f
Korzonkie <rootie0011@gmail.com>
2017-11-08 21:26:17 +0100

    Create simple listView

--- src/client/StoreClient/ViewModel/MainViewModel.cs
+++ src/client/StoreClient/ViewModel/MainViewModel.cs
@@ -1,5 +1,7 @@
 using GalaSoft.MvvmLight;
+using StoreClient.DTOs;
 using StoreClient.Services;
+using System.Collections.ObjectModel;
 using System.Threading.Tasks;
 
 namespace StoreClient.ViewModel
@@ -8,6 +10,17 @@ namespace StoreClient.ViewModel
     {
         private readonly IProductsService productsService;
 
+        private ObservableCollection<Product> products = new ObservableCollection<Product>();
+        public ObservableCollection<Product> Products
+        {
+            get { return products; }
+            set
+            {
+                products = value;
+                RaisePropertyChanged(nameof(Products));
+            }
+        }
+
         public MainViewModel(IProductsService productsService)
         {
             this.productsService = productsService;
@@ -15,6 +28,7 @@ namespace StoreClient.ViewModel
             Task.Run(async () =>
             {
                 var products = await productsService.GetProducts();
+                Products = new ObservableCollection<Product>(products);
             });
         }
     }

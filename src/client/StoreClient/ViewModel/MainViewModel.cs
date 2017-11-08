d63ca17290df4d8923943e4f070f1aeee2c9be7e
Korzonkie <rootie0011@gmail.com>
2017-11-08 20:55:06 +0100

    Integrate with MvvmLight

---
+++ src/client/StoreClient/ViewModel/MainViewModel.cs
@@ -0,0 +1,11 @@
+using GalaSoft.MvvmLight;
+
+namespace StoreClient.ViewModel
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public string Title { get; set; } = "Title";
+        public MainViewModel()
+        { }
+    }
+}

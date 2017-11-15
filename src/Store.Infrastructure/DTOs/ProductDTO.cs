3bef9e8233bf37a7737139c60f474dba2d786a34
Korzonkie <rootie0011@gmail.com>
2017-11-15 14:30:02 +0100

    Add more properties to ProductDTO

--- src/Store.Infrastructure/DTOs/ProductDTO.cs
+++ src/Store.Infrastructure/DTOs/ProductDTO.cs
@@ -5,6 +5,11 @@ namespace Store.Infrastructure.DTOs
     public class ProductDTO
     {
         public Guid Id { get; set; }
-        public string Name { get; set; }
+        public Guid CategoryId { get; private set; }
+        public DateTime DateCreated { get; private set; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public decimal Price { get; private set; }
+        public string ImageUrl { get; private set; }
     }
 }

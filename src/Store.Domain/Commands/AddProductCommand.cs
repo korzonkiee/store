8c65737f8243201edfe82419545b3e20fa129782
Korzonkie <rootie0011@gmail.com>
2017-11-15 15:36:13 +0100

    Include command parameters when creating product

--- src/Store.Domain/Commands/AddProductCommand.cs
+++ src/Store.Domain/Commands/AddProductCommand.cs
@@ -1,12 +1,23 @@
+using System;
+
 namespace Store.Domain.Commands
 {
     public class AddProductCommand : Command
     {
         public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; }
+        public TimeSpan DeliveryTime { get; set; }
 
-        public AddProductCommand(string name)
+        public AddProductCommand(string name, string desc, decimal price,
+            string imageUrl, TimeSpan deliveryTime)
         {
             Name = name;
+            Description = desc;
+            Price = price;
+            ImageUrl = imageUrl;
+            DeliveryTime = deliveryTime;
         }
 
         public override bool IsValid()

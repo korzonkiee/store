52cd85e518594ab374778947e32683c4bbdaad3a
karolektalar <karol@puchaczy.waw.pl>
2017-12-10 17:18:08 +0000

    Scaffold application

--- src/Store.Client.UWP/StoreClient/MainPage.xaml.cs
+++ src/Store.Client.UWP/StoreClient/MainPage.xaml.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using StoreClient.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Devices.Input;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -25,6 +27,37 @@ namespace StoreClient
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private void SortList(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBox cmb = sender as ComboBox;
+            var vm = DataContext as MainViewModel;
+            vm.SortProductsCommand.Execute(cmb.SelectedIndex);
+        }
+
+        //private void AddProduct(object sender, MouseButtonEventArgs e)
+        //{
+        //    Window addProductWindow = new AddProduct();
+        //    addProductWindow.Topmost = true;
+        //    addProductWindow.Show();
+        //}
+
+        private void TextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox tb = (TextBox)sender;
+            tb.Text = string.Empty;
+            tb.GotFocus -= TextBox_GotFocus;
+        }
+
+        private void SearchByName(object sender, MouseEventArgs e)
+        {
+            //var vm = DataContext as MainViewModel;
+            //if (string.IsNullOrEmpty(searchByName.Text))
+            //{
+            //    vm.GetAllProductsCommand.Execute(null);
+            //}
+            //else vm.SearchProductsByNameCommand.Execute(searchByName.Text);
         }
     }
 }
---
+++ src/Store.Client.UWP/StoreClient/Services/CategoriesService.cs
@@ -0,0 +1,25 @@
+﻿using Refit;
+using StoreClient.DTOs;
+using StoreClient.Services.Refit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreClient.Services
+{
+    public interface ICategoriesService
+    {
+        Task<List<Category>> GetCategories();
+    }
+    class CategoriesService : ICategoriesService
+    {
+        public async Task<List<Category>> GetCategories()
+        {
+            return await RestService
+               .For<ICategoriesServiceRefit>("http://localhost:5000")
+               .GetCategories();
+        }
+    }
+}

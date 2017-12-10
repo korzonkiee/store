8a866d13204dbd8e99f3f1f914bf52e44eadae11
karolektalar <karol@puchaczy.waw.pl>
2017-12-10 17:18:08 +0000

    Scaffold application

---
+++ src/Store.Client.UWP/StoreClient/Services/Refit/IProductsServiceRefit.cs
@@ -0,0 +1,27 @@
+﻿using Refit;
+using StoreClient.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreClient.Services.Refit
+{
+    public interface IProductsServiceRefit
+    {
+        [Get("/api/products")]
+        Task<List<Product>> GetProducts();
+
+        [Get("/api/products/1")]
+        Task<List<Product>> GetProductById();
+
+        [Post("/api/products")]
+        Task AddProduct(Product product);
+
+        [Get("/api/products/search/{name}")]
+        Task<List<Product>> SearchProductsByName(String name);
+    }
+
+
+}
